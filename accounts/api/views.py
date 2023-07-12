5f05b71cc0302b89c128e7b0cdd8ff9837db9b32
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 15:24:32 +0200

    Updated accounts/api/views.py added a logout class, update also accounts/api/urls.py added logout url

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, status
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .permissions import IsOwner
 from .serializers import UserSerializer
 
@@ -15,3 +18,11 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        # Remove the token from the user's request object
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
