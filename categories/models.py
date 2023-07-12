9c7978e0907cb747ccf4d5f7a5cfb7b9a5ebe688
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 15:35:06 +0200

    Updated categories/models.py added a new model called Category, also updated categories/admin.py i  registered the model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,24 @@
+import uuid
+from django.core.validators import validate_slug
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_category')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True, validators=[validate_slug])
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
