cf81421fb253455bb08ea1c727f1b95953bdf2a8
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 23:25:19 +0200

    Updated topics/models.py added a new field category, also updated sqlite

--- topics/models.py
+++ topics/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from categories.models import Category
 User = get_user_model()
 # Create your models here.
 
 class Topic(models.Model):
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_topic')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_topic")
     subject = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
-    last_updated = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
