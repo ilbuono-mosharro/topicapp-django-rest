e2ca38a6756969785aad23fca6cb08fed4c5350c
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 11:21:21 +0200

    I created a new AbstractUser called MyUser on accounts/models.py and add on core/settings.py AUTH_USER_MODEL = 'accounts.MyUser'

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator, validate_image_file_extension
 from django.db import models
 
+
 # Create your models here.
+class MyUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email_confirmation = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to="images/avatars/", validators=[
+        validate_image_file_extension, FileExtensionValidator(['JPEG', 'JPG', 'PNG']),
+    ])
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f'{self.username}'
