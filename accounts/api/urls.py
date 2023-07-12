b03f9151556973a6ca58d9f7d24c812b9226ac99
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 13:56:28 +0200

    Updated accounts/api/urls added a obtain auth token url, updated db sqlite

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken import views as rest_views
 from . import views
 
 app_name = "api_accounts"
 
 urlpatterns = [
     path("accounts/sign-up/", views.SignUp.as_view(), name="sign-up"),
-    path("accounts/<uuid:pk>/", views.UserDetail.as_view(), name="user-rud")
+    path("accounts/<uuid:pk>/", views.UserDetail.as_view(), name="user-rud"),
+    path('api-token-auth/', rest_views.obtain_auth_token),
 ]
