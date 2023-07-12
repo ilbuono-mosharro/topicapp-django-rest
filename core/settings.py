189dfc9ce359ed7560a4d6bb04250e008bc8d277
Marinario <ilbuonomosharro@gmail.com>
2023-07-12 11:17:03 +0200

    Added the apps accounts, categories and topics in the core/settings apps

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'categories.apps.CategoriesConfig',
+    'topics.apps.TopicsConfig',
     'rest_framework',
 ]
 
