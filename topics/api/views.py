c0eb315025a719cc4e9e4e5930eda51732ad2457
Marinario <ilbuonomosharro@gmail.com>
2023-07-13 11:11:05 +0200

    Updated topics/api/views.py i override the methods update and destroy on the class TopicViewSet

--- topics/api/views.py
+++ topics/api/views.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from .permissions import IsOwnerUser
 from .serializers import TopicSerializer
 from ..models import Topic
 
@@ -15,8 +16,18 @@ class TopicViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(starter=self.request.user)
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.starter != request.user:
+            raise PermissionDenied("You do not have permission to perform this action.")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.starter != request.user:
+            raise PermissionDenied("You do not have permission to perform this action.")
+        return super().destroy(request, *args, **kwargs)
 
     # user create a topic, user list ther topics, user delete their topic, user update their topics,
     # list all the topics
     # user can vote
-    # add a new field public or private
